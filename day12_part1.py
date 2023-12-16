100a2d3e6509cf2886504e715d042dc810864fbf
Havish <Havish747@gmail.com>
2023-12-15 23:22:59 -0500

    d /Users/havishmaka/Desktop/code/competitve_programming/AdventOfCode2023 ; /usr/bin/env /usr/local/bin/python3 /Users/havishmaka/.cursor/extensions/ms-python.python-2023.14.0/pythonFiles/lib/python/debugpy/adapter/../../debugpy/launcher 53532 -- /Users/havishmaka/Desktop/code/competitve_programming/AdventOfCode2023/day13_part1.py

--- day12_part1.py
+++ day12_part1.py
@@ -1,5 +1,5 @@
 # Open the file called "input.txt" in the same directory as this file.
-input_file = open("input2.txt", 'r')
+input_file = open("input.txt", 'r')
 # Read the file into a list where every element is a line from the file in order.
 # The lines are stored as strings.
 input = input_file.read().strip().split('\n')
